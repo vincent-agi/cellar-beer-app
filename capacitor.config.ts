c7506e60cbff01566317eb379b8f6db9c6abeb8a
vincent agi <contact@followupsystem.fr>
2023-08-31 18:29:02 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cave-a-bieres',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
